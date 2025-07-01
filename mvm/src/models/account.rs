2d7e3ec33d246b4d04bb0172def7cf9342804f93
fawkes <>
2025-07-01 18:33:12 +0800

    feat: block step 1

--- mvm/src/models/account.rs
+++ mvm/src/models/account.rs
@@ -1,7 +1,8 @@
 use consensus::types::{AccountKey, ActionHash};
 use serde::{Deserialize, Serialize};
+use alloy_rlp::{RlpDecodable, RlpEncodable};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, RlpEncodable, RlpDecodable)]
 pub struct Account {
     pub key: AccountKey,
     pub balance: u128,
--- mvm/src/models/transfer.rs
+++ mvm/src/models/transfer.rs
@@ -1,13 +1,14 @@
+use alloy_rlp::{RlpDecodable, RlpEncodable};
 use consensus::types::{AccountKey, ActionHash, Signature};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, RlpEncodable, RlpDecodable)]
 pub struct Transfer {
     pub inner: TransferInner,
     pub from_signature: Signature,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, RlpEncodable, RlpDecodable)]
 pub struct TransferInner {
     pub from: AccountKey,
     pub to: AccountKey,
@@ -16,14 +17,14 @@ pub struct TransferInner {
     pub gas_price: u128,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, RlpEncodable, RlpDecodable)]
 pub struct Merge {
     pub inner: MergeInner,
     pub from_signature: Signature,
     pub to_signature: Signature,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, RlpEncodable, RlpDecodable)]
 pub struct MergeInner {
     pub from: AccountKey,
     pub to: AccountKey,
