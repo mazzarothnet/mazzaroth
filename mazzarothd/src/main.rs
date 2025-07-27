211a68788e7c68baa38e269c2f9e8192bdcffa6f
fawkesLi <fakwes_li@outlook.com>
2025-07-27 23:01:35 +0800

    feat: mathd

--- mazzarothd/src/main.rs
+++ mazzarothd/src/main.rs
@@ -1,5 +1,10 @@
 use utils::log::init_log;
 
-fn main() {
+pub mod gossip;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     init_log();
+
+    Ok(())
 }
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -4,7 +4,6 @@ pub mod log;
 pub mod sha256;
 pub mod time;
 pub mod secp;
-pub mod gossip;
 
 pub fn get_u8_vec_sum(vec: &[u8]) -> u64 {
     vec.iter().map(|x| u64::from(*x)).sum()
