6478f2e1c3f3fcad5e28578d28980d14819cf14a
fawkes <>
2025-08-11 14:29:41 +0800

    feat: use libp2p

--- mazzarothd/src/lib.rs
+++ mazzarothd/src/lib.rs
@@ -1,12 +1,5 @@
 pub mod api;
-pub mod gossip;
 pub mod state;
 
 const MAZZAROTH_HTTP_PORT: &str = "MAZZAROTH_HTTP_PORT";
 const MAZZAROTH_HTTP_PORT_DEFAULT: &str = "8080";
-
-const MAZZAROTH_UDP_PORT: &str = "MAZZAROTH_UDP_PORT";
-const MAZZAROTH_UDP_PORT_DEFAULT: &str = "8081";
-
-const SEED_NODE_ADDR: &str = "SEED_NODE_ADDR";
-const SEED_NODE_ADDR_DEFAULT: &str = "127.0.0.1:8081";
